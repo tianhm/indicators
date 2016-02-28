a98cd89649a7d03bcb1756a2ccd84ed7c102cf46
Oliver Oberdorf <oly@oberdorf.org>
2016-02-28 10:53:55 -0500

    whitespace change so I can push from PyCharm to github

--- src/indicators/ehlers.py
+++ src/indicators/ehlers.py
@@ -22,5 +22,4 @@ class Fisher(MultiMetricMetric):
                        (self.highest.value() - self.lowest.value()) - 0.5) \
             + 0.5 * self.lastValue
         self.lastValue = v
-
         return v
